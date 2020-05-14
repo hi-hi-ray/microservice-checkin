5f3dd83af14b670e195d17f1eb3e5f26c66abefd
hi-hi-ray <dutra.raysa@gmail.com>
2020-05-14 19:41:04 -0300

    remove flasggwer and finish sqs

--- app/api_sync.py
+++ app/api_sync.py
@@ -1,63 +1,16 @@
 from flask import Flask, jsonify, request
-from flasgger import Swagger
-import ticker as check_module
 import sync as syncer
 
 app = Flask(__name__)
-swagger = Swagger(app)
 
 
 @app.route('/health-check/', methods={'GET'})
 def health_check_get():
-    """This endpoint shows that the application it`s up.
-    ---
-    tags:
-      - health check
-    definitions:
-      message:
-        type: string
-    responses:
-      200:
-        description: A message will appear
-        schema:
-        examples:
-          message: 'I`m well and alive, thanks for asking.'
-    """
     return jsonify({"message": "I`m well and alive, thanks for asking."})
 
 
 @app.route('/send', methods={'POST'})
 def add_sqs():
-    """Create your check-in
-  ---
-  tags:
-    - check-in / ticker
-  parameters:
-    - in: body
-      name: body
-      description: JSON parameters.
-      schema:
-        properties:
-          type_stop:
-            type: string
-            description: Type of stop.
-            example: Bus
-          id_stop:
-            type: string
-            description: Id of stop.
-            example: Bus Jacaparepagua1
-  responses:
-    500:
-      description: Failed in application.
-      schema:
-      examples:
-        message: 'a message will appear'
-    200:
-      description: Ok and an array of your checks.
-      schema:
-      examples:
-        message: 'Check-in made'
-  """
     request_data = request.get_json()
     syncer.send_to_sqs(request_data['id'],
                        request_data['type_stop'],
@@ -67,57 +20,10 @@ def add_sqs():
     return jsonify({"message": "Check-in made"}), 200
 
 
-
 @app.route('/sync', methods={'GET'})
-def check_get(id):
-    """Get your check-in
-    ---
-    tags:
-      - check-in / ticker
-    parameters:
-      - in: path
-        name: id
-        description: Path parameter.
-        schema:
-          properties:
-            id:
-              type: string
-              required: true
-              description: Id to search.
-              example: 1
-    definitions:
-      checks:
-        type: object
-        properties:
-          checks:
-            type: array
-            items:
-              id : string
-              timestamp: timestamp
-              type_stop: string
-              id_stop: string
-      id:
-        type: string
-      timestamp:
-        type: timestamp
-      type_stop:
-        type: string
-      id_stop:
-        type: string
-    responses:
-      500:
-        description: Failed in application.
-        schema:
-        examples:
-          message: 'a message will appear'
-      200:
-        description: Ok and an array of your check-in.
-        schema:
-        examples:
-          checks: "#/ref/definitions/checks"
-    """
-    check = check_module.get_checks_by_id(id)
-    return jsonify(check), 200
+def check_get():
+    messages = syncer.get_from_sqs()
+    return jsonify(messages), 200
 
 
 if __name__ == '__main__':
--- app/sync.py
+++ app/sync.py
@@ -7,59 +7,34 @@ def send_to_sqs(id, type_stop, id_stop, type_alert, timestamp=None):
                             aws_access_key_id="AKIAWJLET7HYW67KFCPO",
                             aws_secret_access_key="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",
                             endpoint_url='https://sqs.sa-east-1.amazonaws.com/432392108529')
-    # data = {'id': id, 'type_stop': type_stop, 'id_stop': id_stop, 'type_alert': type_alert, 'timestamp': timestamp}
     queue_name = 'tickers'
     queue = client.get_queue_by_name(QueueName=queue_name)
 
-    message = 'The ticker {0} at {1} in the stop {2} with id {3} had his {4} maded'.format(id, timestamp,
+    message = 'The ticker {0} at {1} in the stop {2} with id {3} has the checkin {4}'.format(id, timestamp,
                                                                                  type_stop, id_stop, type_alert)
-    #
-    # response = queue.send_message(
-    #     MessageBody=message
-    # )
 
-    response = queue.send_messages(Entries=[
-        {
-            'Id': '1',
-            'MessageBody': 'world'
-        },
-        {
-            'Id': '2',
-            'MessageBody': 'boto3',
-            'MessageAttributes': {
-                'Author': {
-                    'StringValue': 'Daniel',
-                    'DataType': 'String'
-                }
-            }
-        }
-    ])
+    response = queue.send_message(MessageBody=message)
 
     print(response)
 
 
 def get_from_sqs():
-    client = boto3.resource('sqs', region_name='sa-east-1',
+    client = boto3.client('sqs', region_name='sa-east-1',
                             aws_access_key_id="AKIAWJLET7HYW67KFCPO",
                             aws_secret_access_key="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",
                             endpoint_url='https://sqs.sa-east-1.amazonaws.com/432392108529')
     # data = {'id': id, 'type_stop': type_stop, 'id_stop': id_stop, 'type_alert': type_alert, 'timestamp': timestamp}
     queue_url = 'https://sqs.sa-east-1.amazonaws.com/432392108529/tickers'
-
+    queue_name = 'tickers'
     response = client.receive_message(
         QueueUrl=queue_url,
         AttributeNames=[
-            'SentTimestamp'
-        ],
-        MaxNumberOfMessages=1,
-        MessageAttributeNames=[
             'All'
-        ],
-        VisibilityTimeout=0,
-        WaitTimeSeconds=0
+        ]
     )
 
     print(response)
+    return response
 
 
 
