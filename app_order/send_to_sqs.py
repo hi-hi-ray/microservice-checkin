c436829c3e71020032f44ad9df696912d7ba0f68
Raysa Dutra <raysadutra.dev@gmail.com>
2021-03-09 08:13:46 -0300

    Remove sensitive data
    O.B.S.: The amazon account for this project doesn't exists anymore.

--- app_order/send_to_sqs.py
+++ app_order/send_to_sqs.py
@@ -3,9 +3,9 @@ import boto3
 
 def send_to_sqs(id, id_toy, quantity):
     client = boto3.resource('sqs', region_name='sa-east-1',
-                            aws_access_key_id="AKIAWJLET7HYQRUDAHFU",
-                            aws_secret_access_key="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",
-                            endpoint_url='https://sqs.sa-east-1.amazonaws.com/432392108529/ordertoystock')
+                            aws_access_key_id="X",
+                            aws_secret_access_key="X",
+                            endpoint_url='https://sqs.sa-east-1.amazonaws.com/X/ordertoystock')
     queue_name = 'ordertoystock'
     queue = client.get_queue_by_name(QueueName=queue_name)
 
